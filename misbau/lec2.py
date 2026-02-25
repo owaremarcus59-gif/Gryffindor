ed6ae028f74ce0d77753d208549497f1cee037e4
rmmisbau111-byte <rmmisbau111@gmail.com>
2026-02-24 22:15:25 -0800

    Misbau: lesson

---
+++ misbau/lec2.py
@@ -0,0 +1,39 @@
+# import sys
+# print(sys.argv)
+# print(sys.version)
+
+"""
+Raw import eg:
+from sys import argv,exit
+
+"""
+
+# from sys import argv,exit
+# a = "name"
+# b = "value"
+# if a==b 
+
+# import sys
+# def main():
+
+# def my_sys_fun():
+    
+#     for arg in arguments[1:]:
+#         if argv == "-m" or argv == "module"
+#             print("module") 
+#         elif arg == "-p" or arg == "path"
+#             print("path")
+#         else:
+#             print("too few arguments")         
+
+# # files
+# open("./text.txt")
+# open("../lecture4/test.txt")
+
+# file = open("./text.txt","w")
+# file.write("append")
+# file.close()
+
+# file = open("./text.txt","w")
+# file.write("Mohammed Misbau")
+# file.close()
