c059bcd96ae97161eef3e479edb9660a036a835b
Estherriches <estherriches286@gmail.com>
2026-02-28 14:52:23 +0000

    Esther:class lesson

---
+++ esther_riches/lecture_4/leature6.py
@@ -0,0 +1,16 @@
+fd=open('./mock.csv')
+
+content=fd.readlines()
+for line in fd.readlines()[:50]:
+    
+    fd.closed()
+    for line in content:
+        print(line)
+
+
+
+
+
+        
+
+#from PIL import Image, ImageChops
